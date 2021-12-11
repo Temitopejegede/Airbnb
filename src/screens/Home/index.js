d3444f16f54a0cf280636b5126dfa0bd420ee024
Temitope Jegede <temiteejegede@gmail.com>
2021-12-11 15:33:55 -0500

    commit

---
+++ src/screens/Home/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+    View, 
+    ImageBackground, 
+    Text, 
+    Pressable,
+} from 'react-native';
+import styles from './styles'
+import { Feather } from '@expo/vector-icons';
+
+const HomeScreen = (props) => {
+    return (
+        <View>
+            <Pressable
+                    style={styles.searchButton}
+                    onPress={() => console.warn('searchButton has been clicked')}
+                >
+                    <Feather name="search" size={24} color="#f15454" />
+                    <Text style={styles.searchButtonText}>Where are you going</Text>
+                    
+            </Pressable>
+
+            <ImageBackground 
+                source={require('../../../assests/images/wallpaper.jpg')} 
+                style={styles.image}
+            >
+                <Text style={styles.title}>Go Near</Text>
+
+                <Pressable
+                    style={styles.button}
+                    onPress={() => console.warn('Explore Btn clicked')}
+                >
+                    
+                    <Text style={styles.buttonText}>Explore nearby stays</Text>
+                    
+                </Pressable>
+            </ImageBackground>
+        </View>
+    );
+};
+
+export default HomeScreen;
