00234d2ddd251a54517f5da8eb529ef68744390c
Temitope Jegede <temiteejegede@gmail.com>
2021-12-15 23:06:12 -0500

    stylesheet corrections

--- src/screens/DestinationScreen/index.js
+++ src/screens/DestinationScreen/index.js
@@ -1,17 +1,21 @@
 import React, {useState} from 'react';
-import {View, TextInput, FlatList} from 'react-native';
-import styles from './styles.js';
+import {
+    View,
+    TextInput, 
+    FlatList
+} from 'react-native';
+import styles from './styles';
 import searchResults from '../../../assests/data/search';
 import {Entypo} from '@expo/vector-icons';
 
 const DestinationSearchScreen = props => {
-  const [inputText, setInputText] = useState('');
-
+{/*const [inputText, setInputText] = useState({initialState: ''});*/}
+const [inputText, setInputText] = useState('');
   return (
     <View style={styles.container}>
       <TextInput
         style={styles.textInput}
-        placeholder="Where are you going"
+        placeholder="Where are you going?"
         value={inputText}
         onChangeText={setInputText}
       />
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -40,3 +40,4 @@ const HomeScreen = (props) => {
 };
 
 export default HomeScreen;
+
