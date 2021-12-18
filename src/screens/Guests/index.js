44aa27f98095e65ba9d48a726f83133e84d6fc77
Temitope Jegede <temiteejegede@gmail.com>
2021-12-17 23:40:16 -0500

    GuestsScreen

---
+++ src/screens/Guests/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from "react-native"
+
+const GuestsScreen = (props) => {
+    return (
+        <View>
+            <Text>notJust a components</Text>
+        </View>
+    );
+};
+
+export default GuestsScreen;
