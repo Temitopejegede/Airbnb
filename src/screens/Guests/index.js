42056cd72a40123ce5e0a1edeeedae9ccdf05381
Temitope Jegede <temiteejegede@gmail.com>
2021-12-20 12:55:29 -0500

    Guest Screen complete

--- src/screens/Guests/index.js
+++ src/screens/Guests/index.js
@@ -1,12 +1,83 @@
-import React from 'react';
-import { View, Text } from "react-native"
+import React, {useState} from 'react';
+import {View, Text, Pressable} from 'react-native';
+import styles from './styles';
 
-const GuestsScreen = (props) => {
-    return (
+const GuestsScreen = props => {
+  const [Adults, setAdults] = useState(0);
+  const [children, setChildren] = useState(0);
+  const [infants, setInfants] = useState(0);
+  return (
+    <View>
+      <View style={styles.row}>
         <View>
-            <Text>notJust a components</Text>
+          <Text style={styles.age}>Adults</Text>
+          <Text style={styles.askAge}>Ages 13 or above</Text>
         </View>
-    );
+
+        <View style={styles.counters}>
+          <Pressable
+            onPress={() => setAdults(Math.max(0, Adults - 1))}
+            style={styles.button}>
+            <Text style={styles.signs}>-</Text>
+          </Pressable>
+
+          <Text style={styles.numbers}>{Adults}</Text>
+
+          <Pressable
+            onPress={() => setAdults(Adults + 1)}
+            style={styles.button}>
+            <Text style={styles.signs}>+</Text>
+          </Pressable>
+        </View>
+      </View>
+
+      <View style={styles.row}>
+        <View>
+          <Text style={styles.age}>Children</Text>
+          <Text style={styles.askAge}>Ages 2 - 12</Text>
+        </View>
+
+        <View style={styles.counters}>
+          <Pressable
+            onPress={() => setChildren(Math.max(0, children - 1))}
+            style={styles.button}>
+            <Text style={styles.signs}>-</Text>
+          </Pressable>
+
+          <Text style={styles.numbers}>{children}</Text>
+
+          <Pressable
+            onPress={() => setChildren(children + 1)}
+            style={styles.button}>
+            <Text style={styles.signs}>+</Text>
+          </Pressable>
+        </View>
+      </View>
+
+      <View style={styles.row}>
+        <View>
+          <Text style={styles.age}>Infants</Text>
+          <Text style={styles.askAge}>Under 2</Text>
+        </View>
+
+        <View style={styles.counters}>
+          <Pressable
+            onPress={() => setInfants(Math.max(0, infants - 1))}
+            style={styles.button}>
+            <Text style={styles.signs}>-</Text>
+          </Pressable>
+
+          <Text style={styles.numbers}>{infants}</Text>
+
+          <Pressable
+            onPress={() => setInfants(infants + 1)}
+            style={styles.button}>
+            <Text style={styles.signs}>+</Text>
+          </Pressable>
+        </View>
+      </View>
+    </View>
+  );
 };
 
 export default GuestsScreen;
--- src/screens/Guests/styles.js
+++ src/screens/Guests/styles.js
@@ -1,5 +1,38 @@
 import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
-    
+    row: { 
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 20,
+        marginHorizontal: 20,
+        borderBottomWidth: 1,
+        borderColor: 'lightgrey',     
+    },
+    button: {
+        borderWidth: 1,
+        width: 30,
+        height: 30,
+        borderRadius: 15,
+        borderColor: 'lightgrey',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    age: {
+        fontWeight: 'bold',
+    },
+    askAge: {
+        color: '#8d8d8d',
+    },
+    counters: {
+        flexDirection: 'row', alignItems: 'center',
+    },
+    signs: {
+        fontsize: 20, color: '#474747',
+    },
+    numbers: {
+        marginHorizontal: 20, fontSize: 16,
+    },
 });
+
+export default styles;
